186a52ef63d2939e019dfe00e3e773b1c545e58a
Nacho <isasmendi.ignacio@gmail.com>
2024-05-09 14:01:12 -0300

    multipages added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import puppeteer from "puppeteer";
 import { performance } from "perf_hooks";
+import { get } from "http";
+import log from "node-gyp/lib/log";
 
 const getQuotes = async () => {
-  //const startTime = performance.now();
   const browser = await puppeteer.launch({headless:true, executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'}) 
   const page = await browser.newPage()
   await page.setRequestInterception(true);
@@ -20,67 +21,35 @@ const getQuotes = async () => {
     waitUntil: "domcontentloaded",
   })
 
-  const items = await page.evaluate(() => {
-    const items = document.querySelectorAll(".ui-search-link__title-card")
-    const links = [] 
-    items.forEach(item => {
-      const link = item.href
-      links.push(link)
-    })
-    return links
-  })
-
-   let links = items.filter(item => !item.includes('click1'))
- 
- /*  await page.goto(links[0], { waitUntil: "domcontentloaded" });
-  const title = await page.evaluate(() => {
-      const title = document.querySelector(".ui-pdp-title").innerText;
-      return title;
-  });
- */
-
-  for (const link of links) {
-    try {
-        await page.goto(link, { waitUntil: "domcontentloaded" });
-        const details = await page.evaluate(() => {
-           // const div = document.querySelector(".ui-pdp-component-list");
-            const title = document.querySelector(".ui-pdp-title").innerText;
-            let sales = document.querySelector(".ui-pdp-subtitle").innerText;
-            sales = sales.match(/\d+/)[0];
-            let price = document.querySelector(".andes-money-amount__fraction").innerText;
-            return { title, sales, price };
-        });
-        console.log(details);
-    } catch (error) {
-        console.error("Error navigating to URL:", link);
-        console.error(error);
-    }
+  async function getLinks(selector) {
+    const links = await page.evaluate(() => {
+      const items = document.querySelectorAll(selector);
+      const links = [];
+      items.forEach((item) => {
+        const link = item.href;
+        links.push(link);
+      });
+      return links;
+    });
+  
+    return links;
   }
 
- // await page.close();
-  await browser.close();
+  let pages = getLinks(".andes-pagination__link")
+  console.log(pages)
 
-  const endTime = performance.now();
-  //console.log(`Execution time: ${Math.floor(endTime - startTime)} ms`);
-  
-}
-
-
-getQuotes()
-
-
-
-
- /*
+  for (const page of pages) {
+    
+    await page.goto(page, { waitUntil: "domcontentloaded" });  
+    let links = getLinks(".ui-search-link__title-card").filter(item => !item.includes('click1'))
     for (const link of links) {
       try {
           await page.goto(link, { waitUntil: "domcontentloaded" });
           const details = await page.evaluate(() => {
-              const div = document.querySelector(".ui-pdp-component-list");
-              const title = div.querySelector(".ui-pdp-title").innerText;
-              let sales = div.querySelector(".ui-pdp-subtitle").innerText;
+              const title = document.querySelector(".ui-pdp-title").innerText;
+              let sales = document.querySelector(".ui-pdp-subtitle").innerText;
               sales = sales.match(/\d+/)[0];
-              let price = div.querySelector(".andes-money-amount__fraction").innerText;
+              let price = document.querySelector(".andes-money-amount__fraction").innerText;
               return { title, sales, price };
           });
           console.log(details);
@@ -89,4 +58,12 @@ getQuotes()
           console.error(error);
       }
     }
-  */
+  }
+  
+  await page.close();
+  await browser.close();
+  
+}
+
+
+getQuotes()
